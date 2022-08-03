adf80a9c731ff0ff3a57803f2ffa41e6c922746c
Jim Robinson <jimbo2150@gmail.com>
2022-08-03 08:45:14 -0400

    Added docblocks

--- src/Loader/Extensions.php
+++ src/Loader/Extensions.php
@@ -8,8 +8,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 abstract class Extensions {
 
     const PREFIX = 'iikiti\\extension\\';
-
-    public static function load(ContainerInterface $container) {
+    
+    /**
+     * Acquires list of enabled extensions and loads them via PSR-4 then
+     * creates a new instance.
+     *
+     * @param  ContainerInterface $container
+     * @return void
+     */
+    public static function load(ContainerInterface $container): void {
         $site = SiteRegistry::getCurrentSite();
         $extAry = $site->getEnabledExtensions();
         foreach($extAry as $ext) {
@@ -21,7 +28,13 @@ abstract class Extensions {
             }
         }
     }
-
+    
+    /**
+     * Adds the path to the extension to the PSR-4 loader.
+     *
+     * @param  \SplFileInfo $path
+     * @return string FQDN of class.
+     */
     protected static function _loadExtensionPsr4(\SplFileInfo $path): string {
         $extName = basename($path->getPathname());
         $vendor = basename($path->getPath());
@@ -31,7 +44,15 @@ abstract class Extensions {
         self::_loadExtensionPsr4_composer($extPrefix, $extPath);
         return $extPrefix . ucfirst($extName) . 'Extension';
     }
-
+    
+    /**
+     * Attempts to load any composer file PSR-4 paths as long as they have the
+     * proper extension namespace prefix.
+     *
+     * @param string $extPrefix Prefix of extension class.
+     * @param string $extPath Full path to extension.
+     * @return void
+     */
     protected static function _loadExtensionPsr4_composer(
         string $extPrefix,
         string $extPath
