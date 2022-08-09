08337e1feffd4f3e13b43aa321ccfec40aa19660
Jim Robinson <jimbo2150@gmail.com>
2022-08-09 12:47:08 -0400

    Corrected missing loader

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+define('PSR4LOADER', require dirname(__DIR__) . '/vendor/autoload.php');
+
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
