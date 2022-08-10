4dd0dbda3bf056ca34854c0164e860f70b0fabff
Jim Robinson <jimbo2150@gmail.com>
2022-08-10 13:01:39 -0400

    Add routes loading from extensions

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use iikiti\CMS\Loader\Extensions;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use ReflectionClass;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 if(!defined('BASE_DIR')) {
     define('BASE_DIR', dirname(__DIR__));
@@ -28,6 +30,31 @@ class Kernel extends BaseKernel
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
         $this->kernelConfigureRoutes($routes);
+        $this->_configureExtensionRoutes($routes);
+    }
+
+    protected function _configureExtensionRoutes(RoutingConfigurator $routes): void {
+        foreach(Extensions::getExtensions() as $ext) {
+            /** @var AbstractBundle $ext */
+            $configDir = dirname(
+                (new ReflectionClass($ext::class))->getFileName()
+            ) . DS . 'config';
+            foreach(
+                array_merge(
+                    glob(
+                        $configDir . DS . '{routes}' . DS . $this->environment  . DS .
+                        '*.{php,yaml}',
+                        GLOB_BRACE
+                    ),
+                    glob(
+                        $configDir . DS . '{routes}' . DS . '*.{php,yaml}',
+                        GLOB_BRACE
+                    )
+                ) as $filename
+            ) {
+                $routes->import($filename);
+            }
+        }
     }
 
     public function boot(): void
