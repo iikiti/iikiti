b033b24e14a60c5f84a4050521530460611e2554
Jim Robinson <jimbo2150@gmail.com>
2022-08-19 12:50:55 -0400

    Extensions associated with site

--- src/Loader/Extensions.php
+++ src/Loader/Extensions.php
@@ -5,10 +5,12 @@ namespace iikiti\CMS\Loader;
 use iikiti\ExtensionUtilities\ExtensionInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
 use iikiti\CMS\BASE_DIR;
+use iikiti\CMS\Registry\SiteRegistry;
 
 abstract class Extensions {
 
     const PREFIX = 'iikiti\\extension\\';
+    const INITIAL_KEY = 'initial';
     
     static $EXTENSIONS = [];
     
@@ -20,8 +22,15 @@ abstract class Extensions {
      */
     public static function load(): \Generator {
         $gen = self::_loadFromDirectory();
-        self::$EXTENSIONS[] = $gen->current();
+        self::$EXTENSIONS[
+            SiteRegistry::getCurrentSite()?->getId() ?? self::INITIAL_KEY
+        ][] = $gen->current();
         yield from $gen;
+    }
+
+    public static function setInitialSiteId($siteId) {
+        self::$EXTENSIONS[$siteId] = self::$EXTENSIONS[self::INITIAL_KEY];
+        unset(self::$EXTENSIONS[self::INITIAL_KEY]);
     }
 
     protected static function _loadFromDirectory(): \Generator {
@@ -61,7 +70,9 @@ abstract class Extensions {
     }
 
     public static function getExtensions() {
-        return self::$EXTENSIONS; 
+        return self::$EXTENSIONS[
+            SiteRegistry::getCurrentSite()?->getId() ?? self::INITIAL_KEY
+        ] ?? []; 
     }
 
 }
