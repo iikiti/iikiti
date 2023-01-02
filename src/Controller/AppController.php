45390c7b64de20f9e5c41c1567e7d90d2361c494
Jim Robinson <jimbo2150@gmail.com>
2023-01-02 07:10:49 -0500

    - Added required method that was complained about by composer

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,10 +3,16 @@
 namespace iikiti\CMS\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Container\ContainerInterface;
 
 /**
  * Class AppController
  */
 abstract class AppController extends AbstractController {
+
+    public function setContainer(ContainerInterface $container): ?ContainerInterface
+    {
+        return parent::setContainer($container);
+    }
     
 }
