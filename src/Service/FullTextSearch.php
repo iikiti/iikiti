0689aa0463f49230d3d70d7a606f49a5f9d43ae5
Jim Robinson <jimbo2150@gmail.com>
2023-02-08 15:56:43 -0500

    - Begin adding search engine

---
+++ src/Service/FullTextSearch.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace iikiti\CMS\Service;
+
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use iikiti\CMS\Entity\DbObject;
+
+#[AsEntityListener(
+    event: Events::postPersist,
+    method: 'postPersist',
+    entity: DbObject::class
+)]
+class FullTextSearch {
+
+    public function __construct() {
+
+
+    }
+
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if (!($entity instanceof DbObject)) {
+            return;
+        }
+
+        $entityManager = $args->getObjectManager();
+        
+    }
+}
