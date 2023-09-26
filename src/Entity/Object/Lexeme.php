71dcef95cbcf1e3f60a24029b24f912b1b8485c6
Jim Robinson <jimbo2150@gmail.com>
2023-09-26 18:47:36 -0400

    - Removed discriminator map generator: Doctrine already creates a default one based on subclass names

--- src/Entity/Object/Lexeme.php
+++ src/Entity/Object/Lexeme.php
@@ -2,13 +2,11 @@
 namespace iikiti\CMS\Entity\Object;
 
 use Doctrine\ORM\Mapping as ORM;
-use iikiti\CMS\Doctrine\DiscriminatorValue;
 use iikiti\CMS\Entity\DbObject;
 use iikiti\CMS\Repository\Object\LexemeRepository;
 
 #[ORM\Entity(repositoryClass: LexemeRepository::class)]
 #[ORM\Table(name: "objects")]
-#[DiscriminatorValue('lexeme')]
 class Lexeme extends DbObject
 {
 
