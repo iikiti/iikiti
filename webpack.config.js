20d7b6a02926498a8da9e7ea2f6e100c384c9e02
Jim Robinson <jimbo2150@gmail.com>
2023-09-29 11:18:35 -0400

    - Fixed grammar

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDNs or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 	.enableSvelte()
 	.configureLoaderRule('svelte', svelte => {
