197bb81e7a5391a6b510a1527b0863431fce297a
Jim Robinson <jimbo2150@gmail.com>
2023-09-29 16:26:39 -0400

    - Added svelte to templates

--- assets/main.js
+++ assets/main.js
@@ -3,19 +3,25 @@
 import '/assets/js/utilities/XPathResult.js';
 
 // Get all custom elements
-let ce = Array.from(document.evaluate( "//*[contains(name(),'-')]", document, null, XPathResult.ANY_TYPE, null ));
+let ce = Array.from(document.querySelectorAll("[data-svelte-component]"));
 var ceTagObj = {};
 
 ce.forEach((elem) => {
-	ceTagObj[elem.tagName.toLowerCase()] = true;
-});
-
-Object.keys(ceTagObj).forEach((key) => {
-	// import(
-		/* webpackChunkName: "js/chunks/block-editor-web-components" */
-		/* webpackMode: "lazy" */
-		/* webpackPrefetch: true */
-		/* webpackPreload: true */
-	//	'/assets/js/components/' + key + '.js'
-	//);
+	let tag = "svelteComponent" in elem.dataset && elem.dataset.svelteComponent !== "" ?
+		elem.dataset.svelteComponent :
+		elem.tagName.split(':', 2)[1].toLowerCase();
+	if(!(tag in ceTagObj)) {
+		ceTagObj[tag] = import(
+			/* webpackChunkName: "js/chunks/block-editor-web-components" */
+			/* webpackMode: "lazy" */
+			/* webpackPrefetch: true */
+			/* webpackPreload: true */
+			'/assets/js/components/' + tag + '.svelte'
+		);
+	}
+	ceTagObj[tag].then(((elem, tag, module) => {
+		new module.default({
+			target: elem
+		});
+	}).bind(null, elem, tag));
 });
