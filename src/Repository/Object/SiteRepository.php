50d0980d5ba8e62d707753ea6e3c16178c072f71
Jim Robinson <jimbo2150@gmail.com>
2023-10-16 14:43:04 -0400

    - Fixed associations and added domain find

--- src/Repository/Object/SiteRepository.php
+++ src/Repository/Object/SiteRepository.php
@@ -24,12 +24,25 @@ class SiteRepository extends ObjectRepository
 		/** @var ObjectRepository $propRep */
 		$propQ = $this->getEntityManager()
 			->getRepository(ObjectProperty::class)
-			->createQueryBuilder('p')
+			->createQueryBuilder('p2')
+			->select('p2.object_id')
 			->setParameter(':name', 'domain')
-			->setParameter(':domain', $domain)
-			->andWhere('p.name = :name')
-			->andWhere('JSON_CONTAINS(p.value, :domain, "$")');
-        return [$this->find(1)];
+			->setParameter(':domain', json_encode($domain))
+			->andWhere('p2.name = :name')
+			->andWhere("JSON_CONTAINS(p2.value, :domain) = 1")
+			->orderBy('p2.created')
+			->setMaxResults(1);
+		$siteQ = $this->getEntityManager()
+			->createQuery(
+				'SELECT s ' .
+				'FROM ' . $this->getClassName() . ' s ' .
+					'JOIN s.properties p ' .
+				'WHERE p.name = \'domain\' AND JSON_CONTAINS(p.value, :domain) = 1 AND s.id IN (' .
+					$propQ->getDQL() . ')'
+			)
+			->setParameter(':name', 'domain')
+			->setParameter('domain', json_encode($domain));
+        return $siteQ->getResult();
     }
 
 }
