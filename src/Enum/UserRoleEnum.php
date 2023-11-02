b3c93d12932641d74921c687e8df030b17f5cc56
Jim Robinson <jimbo2150@gmail.com>
2023-11-02 10:07:39 -0400

    - Added new roles and defaults

--- src/Enum/UserRoleEnum.php
+++ src/Enum/UserRoleEnum.php
@@ -5,6 +5,20 @@ namespace iikiti\CMS\Enum;
 
 final class UserRoleEnum extends DynamicBackedEnumerator {
 
+	static protected array $defaultRoles = [];
+
+	public static function registerDefault(string|int $role): void {
+		$role = self::from($role);
+		self::$defaultRoles = array_merge([$role->getName() => $role], self::$defaultRoles);
+	}
+
+	public static function getDefaultRoles(): array {
+		return self::$defaultRoles;
+	}
+
 }
 
-UserRoleEnum::register('USER', 'ROLE_USER');
+UserRoleEnum::register('User', 'ROLE_USER');
+UserRoleEnum::registerDefault('ROLE_USER');
+UserRoleEnum::register('Administrator', 'ROLE_ADMIN');
+UserRoleEnum::register('Super Administrator', 'ROLE_SUPER_ADMIN');
