92cf3de21f95af31981727000790e23af74ecf9f
Jim Robinson <jimbo2150@gmail.com>
2023-11-03 08:45:09 -0400

    - Prevent psalm missing file error

--- src/.preload.php
+++ src/.preload.php
@@ -1,5 +1,6 @@
 <?php
 
 if (file_exists(__DIR__.'/../var/cache/dev/App_KernelDevDebugContainer.preload.php')) {
+    /** @psalm-suppress MissingFile */
     require __DIR__.'/../var/cache/dev/App_KernelDevDebugContainer.preload.php';
 }
