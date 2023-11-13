8cad589c8a29aa612b6c5639025812497098ec67
Jim Robinson <jimbo2150@gmail.com>
2023-11-13 17:30:05 -0500

    - Continued working on multi-factor authentication

--- src/EventSubscriber/MultiFactorAuthenticationTokenListener.php
+++ src/EventSubscriber/MultiFactorAuthenticationTokenListener.php
@@ -5,7 +5,9 @@ namespace iikiti\CMS\EventSubscriber;
 use iikiti\CMS\Security\Authentication\MultiFactorAuthenticationToken;
 use iikiti\CMS\Security\Authentication\MultiFactorTokenInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Event\AuthenticationTokenCreatedEvent;
+use Symfony\Component\Security\Http\Event\CheckPassportEvent;
 
 class MultiFactorAuthenticationTokenListener implements EventSubscriberInterface
 {
