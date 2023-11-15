f97314d60842a2666d128e8faf6d4e56dd448859
Jim Robinson <jimbo2150@gmail.com>
2023-11-15 09:31:25 -0500

    - Added PHP CS-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,7 +9,6 @@ return (new PhpCsFixer\Config())
 	->setRules([
 		'@Symfony' => true,
 		'no_unused_imports' => true,
-		'operator_linebreak' => ['position' => 'end'],
 	])
 	->setFinder($finder)
 	->setIndent("\t")
