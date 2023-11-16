692903cd3aa753509f25c78cf4efa84a7e7b7e0c
Jim Robinson <jimbo2150@gmail.com>
2023-11-16 17:43:06 -0500

    - Fixed database types

--- src/Entity/ObjectProperty.php
+++ src/Entity/ObjectProperty.php
@@ -1,26 +1,25 @@
 <?php
+
 namespace iikiti\CMS\Entity;
 
-use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use iikiti\CMS\Repository\ObjectPropertyRepository;
 
 #[ORM\Entity(repositoryClass: ObjectPropertyRepository::class)]
-#[ORM\Table(name: "object_properties", options: ["where" => "(1 = 1)"])]
+#[ORM\Table(name: 'object_properties', options: ['where' => '(1 = 1)'])]
 class ObjectProperty
 {
-
 	#[ORM\ManyToOne(targetEntity: DbObject::class, inversedBy: 'properties')]
 	private ?DbObject $object;
 
 	#[ORM\Id()]
-    #[ORM\GeneratedValue()]
-    #[ORM\Column(type: Types::BIGINT)]
-	private int $id;
+	#[ORM\GeneratedValue()]
+	#[ORM\Column(type: Types::BIGINT)]
+	private int|string $id;
 
 	#[ORM\Column(type: Types::BIGINT)]
-	private int $object_id;
+	private int|string $object_id;
 
 	#[ORM\Column(type: Types::STRING)]
 	private string $name;
@@ -29,26 +28,30 @@ class ObjectProperty
 	private int|float|string|array $value;
 
 	#[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
-	private DateTimeInterface $created;
+	private \DateTimeInterface $created;
 
-	public function getName(): string {
+	public function getName(): string
+	{
 		return $this->name;
 	}
 
-	public function getValue(): int|float|string|array {
+	public function getValue(): int|float|string|array
+	{
 		return $this->value;
 	}
 
-	public function getCreationDate(): DateTimeInterface {
+	public function getCreationDate(): \DateTimeInterface
+	{
 		return $this->created;
 	}
 
-	public function getId(): int {
+	public function getId(): int|string
+	{
 		return $this->id;
 	}
 
-	public function getObject(): ?DbObject {
+	public function getObject(): ?DbObject
+	{
 		return $this->object;
 	}
-
 }
