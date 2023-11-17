7d75f374428f5dd63046fc63ccb190ed4f587fe2
Jim Robinson <jimbo2150@gmail.com>
2023-11-17 08:27:11 -0500

    - Fixed deprecation warnings

--- src/Security/Authentication/MultiFactorAuthenticationToken.php
+++ src/Security/Authentication/MultiFactorAuthenticationToken.php
@@ -1,83 +1,99 @@
 <?php
+
 namespace iikiti\CMS\Security\Authentication;
 
 use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class MultiFactorAuthenticationToken extends AbstractToken implements MultiFactorTokenInterface {
-
+class MultiFactorAuthenticationToken extends AbstractToken implements MultiFactorTokenInterface
+{
 	/** @psalm-suppress PropertyNotSetInConstructor */
 	private TokenInterface $associatedToken;
 
 	private bool $authenticated = false;
 
-	public function __construct(array $roles = []) {
+	public function __construct(array $roles = [])
+	{
 		parent::__construct($roles);
 	}
 
-	public function getAssociatedToken(): TokenInterface {
+	public function getAssociatedToken(): TokenInterface
+	{
 		return $this->associatedToken;
 	}
 
-	public function setAssociatedToken(TokenInterface $token): void {
+	public function setAssociatedToken(TokenInterface $token): void
+	{
 		$this->associatedToken = $token;
 	}
 
-	public function getUser(): ?UserInterface {
-        return $this->getAssociatedToken()->getUser();
-    }
+	public function getUser(): ?UserInterface
+	{
+		return $this->getAssociatedToken()->getUser();
+	}
 
-	public function setUser(UserInterface $user) {
-        $this->getAssociatedToken()->setUser($user);
-    }
+	public function setUser(UserInterface $user): void
+	{
+		$this->getAssociatedToken()->setUser($user);
+	}
 
-	public function isAuthenticated(): bool {
+	public function isAuthenticated(): bool
+	{
 		return $this->authenticated;
 	}
 
-	public function setIsAuthenticated(bool $authenticated): void {
+	public function setIsAuthenticated(bool $authenticated): void
+	{
 		$this->authenticated = $authenticated;
 	}
 
-	public function getRoleNames(): array {
+	public function getRoleNames(): array
+	{
 		return $this->getAssociatedToken()->getRoleNames();
 	}
 
-	public function getUserIdentifier(): string {
+	public function getUserIdentifier(): string
+	{
 		return $this->getUser()?->getUserIdentifier() ?? '';
 	}
 
-	public function getAttributes(): array {
+	public function getAttributes(): array
+	{
 		return $this->getAssociatedToken()->getAttributes();
 	}
 
-	public function setAttributes(array $attributes) {
+	public function setAttributes(array $attributes): void
+	{
 		$this->getAssociatedToken()->setAttributes($attributes);
 	}
 
-	public function hasAttribute(string $name): bool {
+	public function hasAttribute(string $name): bool
+	{
 		return $this->getAssociatedToken()->hasAttribute($name);
 	}
 
-	public function getAttribute(string $name): mixed {
+	public function getAttribute(string $name): mixed
+	{
 		return $this->getAssociatedToken()->getAttribute($name);
 	}
 
-	public function setAttribute(string $name, mixed $value) {
+	public function setAttribute(string $name, mixed $value): void
+	{
 		$this->getAssociatedToken()->setAttribute($name, $value);
 	}
 
-	public function __serialize(): array {
+	public function __serialize(): array
+	{
 		$data = parent::__serialize();
 		$data[] = $this->associatedToken;
+
 		return $data;
 	}
 
-	public function __unserialize(array $data): void {
+	public function __unserialize(array $data): void
+	{
 		[, , , , , $this->associatedToken] = $data;
 		parent::__unserialize($data);
 	}
-
 }
-
