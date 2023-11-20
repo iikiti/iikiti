70d78040847736c88a2dd2f12d1baff153730193
Jim Robinson <jimbo2150@gmail.com>
2023-11-20 15:56:53 -0500

    - Removed Vue components

--- assets/js/BlockEditor/BlockElementUtilityPanel.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class BlockElementUtilityPanel extends React.Component {
-
-    render() {
-        return <utility-panel is="" class={this.props.side}></utility-panel>;
-    }
-
-}
