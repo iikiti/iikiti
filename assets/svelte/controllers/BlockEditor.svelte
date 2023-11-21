348814c12c91086c5eb2e9f1436b0cfd4c38e67a
Jim Robinson <jimbo2150@gmail.com>
2023-11-21 13:57:08 -0500

    - Continuing work on editor

--- assets/svelte/controllers/BlockEditor.svelte
+++ assets/svelte/controllers/BlockEditor.svelte
@@ -1,7 +1,23 @@
 <script>
 	import { onMount } from 'svelte';
+	import { get_current_component } from 'svelte/internal';
+
+	let node = get_current_component().$$.root;
+
+	let template = node.previousSibling.matches("template[data-for='BlockEditor']") ?
+		get_current_component().$$.root.previousSibling :
+		null;
+	
+	if(!template) {
+		throw new Error("Invalid editor template");
+	}
+
+	let wrapper = template.content.firstElementChild.cloneNode(true);
+
 
 	onMount(function() {
-		console.log("mounted");
+		//console.log("mounted", node);
 	});
 </script>
+
+<div>Test 2</div>
