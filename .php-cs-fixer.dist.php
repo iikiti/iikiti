2e51b3318d97855284f90d12e4ff1c0fde56e518
Jim Robinson <jimbo2150@gmail.com>
2023-12-13 13:48:25 -0500

    - Corrected comments being broken

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,16 +1,17 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-	->in(__DIR__)
-	->exclude('var')
+$finder = (new PhpCsFixer\Finder())->
+	in(__DIR__)->
+	exclude('var')
 ;
 
-return (new PhpCsFixer\Config())
-	->setRules([
+return (new PhpCsFixer\Config())->
+	setRules([
 		'@Symfony' => true,
 		'no_unused_imports' => true,
 		'operator_linebreak' => ['position' => 'end'],
-	])
-	->setFinder($finder)
-	->setIndent("\t")
+		'phpdoc_to_comment' => false,
+	])->
+	setFinder($finder)->
+	setIndent("\t")
 ;
