4e3f25705624bb772aa84416d6ec04a8449dc43b
Jim Robinson <jimbo2150@gmail.com>
2023-12-15 16:58:31 -0500

    - Removed need to suppress error

--- src/Entity/DbObject.php
+++ src/Entity/DbObject.php
@@ -2,6 +2,7 @@
 
 namespace iikiti\CMS\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -35,14 +36,13 @@ class DbObject
 
 	private ?string $type = null;
 
-	/** @psalm-suppress PropertyNotSetInConstructor
-	 * @var Collection<string,ObjectProperty> $properties
-	 */
+	/** @var Collection<string,ObjectProperty> */
 	#[ORM\OneToMany(targetEntity: ObjectProperty::class, mappedBy: 'object', indexBy: 'name')]
 	private Collection $properties;
 
 	public function __construct()
 	{
+		$this->properties = new ArrayCollection();
 	}
 
 	public function getId(): int|string|null
