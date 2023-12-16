cf2ad0c79636bbf08d759ffe5e132cdf71f55bb7
Jim Robinson <jimbo2150@gmail.com>
2023-12-16 07:36:19 -0500

    - Removed unnecessary docblock

--- src/Controller/Page/AdminController.php
+++ src/Controller/Page/AdminController.php
@@ -10,22 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 #[AsController]
 class AdminController extends AppController
 {
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    #[Route('/admin', name: "admin_home")]
-    function home(): Response {
-        return $this->render(
-            'index.twig',
-            [
-                'doc' => ['title' => 'iikiti']
-            ],
-            new Response(
-                '',
-                Response::HTTP_OK,
-                ['content-type' => 'text/html']
-            )
-        );
-    }
-
+	#[Route('/admin', name: 'admin_home')]
+	public function home(): Response
+	{
+		return $this->render(
+			'index.twig',
+			[
+				'doc' => ['title' => 'iikiti'],
+			],
+			new Response(
+				'',
+				Response::HTTP_OK,
+				['content-type' => 'text/html']
+			)
+		);
+	}
 }
