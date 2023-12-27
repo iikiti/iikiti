64e2c0ac55ea2e8cc7aef80b9cafbdc46e3f3e90
Jim Robinson <jimbo2150@gmail.com>
2023-12-27 14:15:06 -0500

    - Removed MFA files (moving to bundle)

--- src/Security/Authentication/MultiFactor/AccessDeniedException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace iikiti\CMS\Security\Authentication\MultiFactor;
-
-use Symfony\Component\Security\Core\Exception\AccessDeniedException as SecurityAccessDeniedException;
-
-class AccessDeniedException extends SecurityAccessDeniedException
-{
-}
