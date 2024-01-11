d2c3963563d532f7f528e671558907451ae4f984
Jim Robinson <jimbo2150@gmail.com>
2024-01-11 16:59:05 -0500

    - Formatting changes

--- src/Controller/Page/HomeController.php
+++ src/Controller/Page/HomeController.php
@@ -9,28 +9,24 @@ use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class AppController
+ * Class AppController.
  */
 #[AsController]
-class HomeController extends AppController {
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    #[Route('/', name: "home", priority: 0)]
-    function index(EntityManagerInterface $em): Response
-    {
-        return $this->render(
-            'index.twig',
-            [
-                'doc' => ['title' => 'iikiti']
-            ],
-            new Response(
-                '',
-                Response::HTTP_OK,
-                ['content-type' => 'text/html']
-            )
-        );
-    }
-
+class HomeController extends AppController
+{
+	#[Route('/', name: 'home', priority: 0)]
+	public function index(EntityManagerInterface $em): Response
+	{
+		return $this->render(
+			'index.twig',
+			[
+				'doc' => ['title' => 'iikiti'],
+			],
+			new Response(
+				'',
+				Response::HTTP_OK,
+				['content-type' => 'text/html']
+			)
+		);
+	}
 }
