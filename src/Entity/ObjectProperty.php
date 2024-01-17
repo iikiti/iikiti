9ead1a77d80aea6a77fa7e20b1aec8d82af1eb80
Jim Robinson <jimbo2150@gmail.com>
2024-01-17 14:01:08 -0500

    - Added template (not currently fully working due to possible bug in psalm) - Added default property values - Added constructor - Added setter methods

--- src/Entity/ObjectProperty.php
+++ src/Entity/ObjectProperty.php
@@ -7,45 +7,78 @@ use Doctrine\ORM\Mapping as ORM;
 use iikiti\CMS\Repository\ObjectPropertyRepository;
 
 #[ORM\Entity(repositoryClass: ObjectPropertyRepository::class)]
-#[ORM\Table(name: 'object_properties', options: ['where' => '(1 = 1)'])]
+#[ORM\Table(name: 'object_properties')]
+/**
+ * @template T of int|float|string|array|null
+ */
 class ObjectProperty
 {
 	#[ORM\ManyToOne(targetEntity: DbObject::class, inversedBy: 'properties')]
-	private ?DbObject $object;
+	private ?DbObject $object = null;
 
 	#[ORM\Id()]
 	#[ORM\GeneratedValue()]
 	#[ORM\Column(type: Types::BIGINT)]
-	private int|string $id;
+	private int|string|null $id = null;
 
 	#[ORM\Column(type: Types::BIGINT)]
-	private int|string $object_id;
+	private int|string|null $object_id = null;
 
 	#[ORM\Column(type: Types::STRING)]
-	private string $name;
+	private string|null $name = null;
 
 	#[ORM\Column(type: Types::JSON)]
-	private int|float|string|array $value;
+	/** @var T */
+	private int|float|string|array|null $value = null;
 
 	#[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
 	private \DateTimeInterface $created;
 
-	public function getName(): string
+	public function __construct(
+		DbObject $dbObject = null,
+		string $name = null,
+		mixed $value = null
+	) {
+		$this->created = new \DateTimeImmutable();
+		$this->object = $dbObject;
+		$this->setName($name);
+		$this->setValue($value);
+	}
+
+	public function getName(): string|null
 	{
 		return $this->name;
 	}
 
-	public function getValue(): int|float|string|array
+	public function setName(string|null $name): void
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * @return T
+	 */
+	public function getValue(): mixed
 	{
 		return $this->value;
 	}
 
-	public function getCreationDate(): \DateTimeInterface
+	public function setValue(mixed $value): void
+	{
+		$this->value = $value;
+	}
+
+	public function getCreationDate(): ?\DateTimeInterface
 	{
 		return $this->created;
 	}
 
-	public function getId(): int|string
+	public function setCreationDate(\DateTimeInterface $created): void
+	{
+		$this->created = $created;
+	}
+
+	public function getId(): int|string|null
 	{
 		return $this->id;
 	}
