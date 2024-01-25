07deb4ed53a9fe5fb03590621fec4b00ce512e71
Jim Robinson <jimbo2150@gmail.com>
2024-01-25 15:43:27 -0500

    - Updated interface name after bundle update

--- src/Entity/Object/User.php
+++ src/Entity/Object/User.php
@@ -7,13 +7,13 @@ use iikiti\CMS\Entity\DbObject;
 use iikiti\CMS\Entity\ObjectProperty;
 use iikiti\CMS\Manager\UserRoleManager;
 use iikiti\CMS\Repository\Object\UserRepository;
-use iikiti\MfaBundle\Entity\UserInterface as MfaUserInterface;
+use iikiti\MfaBundle\Authentication\Interface\MfaPreferencesInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'objects')]
-class User extends DbObject implements PasswordAuthenticatedUserInterface, MfaUserInterface, UserInterface
+class User extends DbObject implements PasswordAuthenticatedUserInterface, MfaPreferencesInterface, UserInterface
 {
 	public const SITE_SPECIFIC = false;
 
