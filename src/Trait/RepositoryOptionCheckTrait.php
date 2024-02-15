64ae0e59954e919149b6f4b2a0c635c337d842c2
Jim Robinson <jimbo2150@gmail.com>
2024-02-15 13:22:32 -0500

    - Moved some code into trait

---
+++ src/Trait/RepositoryOptionCheckTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace iikiti\CMS\Trait;
+
+trait RepositoryOptionCheckTrait
+{
+	protected function _typeCheck_bool($value, $allowNull = true): bool
+	{
+		return is_bool($value) || (null === $value && $allowNull);
+	}
+
+	protected function _typeCheck_stringOrArray($value, $allowNull = true): bool
+	{
+		return (is_string($value) || is_array($value)) || (null === $value && $allowNull);
+	}
+
+	protected static function _checkOption(
+		string $key,
+		array $options,
+		\Closure|null $typeCheck
+	): mixed {
+		$default = self::_defaultOption($key);
+		$value = $options[$key] ?? null;
+		if (null !== $typeCheck && !$typeCheck($value)) {
+			throw new \InvalidArgumentException('Type is incorrect for key: '.$key);
+		}
+
+		return $value ?? $default;
+	}
+
+	protected static function _defaultOption(string $key): bool|string|null
+	{
+		static $values = [
+			'filterBySite' => true,
+			'includeRevisions' => false,
+		];
+
+		return $values[$key] ?? null;
+	}
+}
