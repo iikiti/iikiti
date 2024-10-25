27ffa558adc28504d4ffa2e6387783c8319976b7
Jim Robinson <jimbo2150@gmail.com>
2024-10-25 17:05:31 -0400

    - Fixed use of no-longer-available library

--- assets/svelte/controllers/BlockEditor.svelte
+++ assets/svelte/controllers/BlockEditor.svelte
@@ -1,22 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
-	import { get_current_component } from 'svelte/internal';
 
-	var node = get_current_component().$$.root;
-
-	/*
-	console.log(node.previousSibling?.matches('test'));
-	/*
-	console.log(node.previousSibling?.matches('test'));
-	let template = node.previousSibling.matches("template[data-for='BlockEditor']") ?
-		get_current_component().$$.root.previousSibling :
-		null;
-	
-	if(!template) {
-		throw new Error("Invalid editor template");
-	}
-
-	*/
 	console.log('BlockEditor');
 
 	onMount(function() {
