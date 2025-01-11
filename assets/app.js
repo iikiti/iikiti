7961b66131d078f365c5b39ceacf7b10cb361ffb
Jim Robinson <jimbo2150@gmail.com>
2025-01-11 15:52:19 -0500

    - Removed symfony stimulus/ui/ux packages - Fixed deprecation warnings - Upgraded to svelte 5

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,5 @@
-import { registerSvelteControllerComponents } from '@symfony/ux-svelte';
-
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import { mount } from 'svelte';
+import BlockEditor from './svelte/controllers/BlockEditor.svelte';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
@@ -15,4 +9,6 @@ import './bootstrap';
 
 import './main.js';
 
-registerSvelteControllerComponents(require.context('./svelte/controllers', true, /\.svelte$/));
+mount(BlockEditor, {
+	target: document.querySelector('[data-component="BlockEditorComponent"]'),
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
-//const path = require('path');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -36,11 +36,6 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
-
-    .enableSvelte()
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
 
     //.enableVueLoader(() => {}, { runtimeCompilerBuild: false })
 
@@ -99,5 +94,28 @@ webpackConfig.resolve.conditionNames = (webpackConfig.resolve.conditionNames??[]
 if(webpackConfig.resolve.conditionNames.indexOf('svelte') < 0) {
 	webpackConfig.resolve.conditionNames.push('svelte');
 }
+
+webpackConfig.resolve.conditionNames.push('svelte', 'browser');
+
+webpackConfig.module.rules.push(
+	{
+        test: /\.svelte\.ts$/,
+        use: [ "svelte-loader", { loader: "ts-loader", options: { transpileOnly: true } }],
+    },
+    // This is the config for other .ts files - the regex makes sure to not process .svelte.ts files twice
+    {
+        test: /(?<!\.svelte)\.ts$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true, // you should use svelte-check for type checking
+        }
+    },
+    {
+    	test: /\.(svelte|svelte\.js)$/,
+        use: 'svelte-loader'
+    },
+);
+
+//console.log(webpackConfig.module.rules);return;
 
 module.exports = webpackConfig;
