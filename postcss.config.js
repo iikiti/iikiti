85929fb5f7a696fcbffb61c343ec9a528fcf91f4
Jim Robinson <jimbo2150@gmail.com>
2025-01-22 21:06:58 -0500

    - Updated to tailwindcss 4.0

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ let tailwindcss = require('tailwindcss');
 
 module.exports = {
     plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('postcss-import'),
-        require('autoprefixer')
+		{"@tailwindcss/postcss": {}},
     ]
 }
