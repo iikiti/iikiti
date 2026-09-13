d82b9a6c54931081d15b7a04b7985bd401f04361
Jim Robinson <jimbo2150@gmail.com>
2026-09-13 11:45:00 -0400

    - Added ignore for phpstan

--- src/Trait/DisablesDatabaseCache.php
+++ src/Trait/DisablesDatabaseCache.php
@@ -10,6 +10,8 @@ use iikiti\CMS\Service\CacheState;
  * Commands that write many entities (imports, seeds, migrations) should call
  * disableDatabaseCache() so that every query bypasses the cache, avoiding stale
  * reads and excessive cache invalidation during the batch run.
+ * 
+ * @phpstan-ignore trait.unused (Provided by caching layer for developer use)
  */
 trait DisablesDatabaseCache
 {
